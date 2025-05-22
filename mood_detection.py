8cd95d197dd555d26a5a8fefb3754f9f32bb4f58
Max_THERESIAS <Maxally457@outlook.com>
2025-05-23 00:07:44 +0800

    new file:   mood_detection.py new file:   songs.csv

---
+++ mood_detection.py
@@ -0,0 +1,14 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+
+
+tokenizer = AutoTokenizer.from_pretrained("IsmaelMousa/bert-finetuned-emotion")
+model = AutoModelForSequenceClassification.from_pretrained("IsmaelMousa/bert-finetuned-emotion")
+
+def get_emotion(text):
+    inputs = tokenizer(text, return_tensors="pt")
+    outputs = model(**inputs)
+    predicted_class = torch.argmax(outputs.logits, dim=1)
+    return model.config.id2label[predicted_class.item()]
+
+print(get_emotion("I'm feeling very happy today."))
